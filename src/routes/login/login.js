4fd2095dc8645777addcd5c737bd1bb57dd80d64
zchuhui <943339564@qq.com>
2017-11-28 16:03:33 +0800

    登录页面

--- src/routes/login/login.js
+++ src/routes/login/login.js
@@ -16,43 +16,52 @@ class Login extends React.Component {
 
   render() {
     return (
-      <div className={styles.loginWrap}> 
-        <h2 className={styles.logo}><img src={Logo}/></h2>
+      <div className={styles.loginWrap}>
+        <div className={styles.title}>
+          <b>记录、分析、交流梦境</b>
+          <p>
+            连接我们在一起的力量是因为 <br />
+            我们相信梦的趣味、意义、价值
+          </p>
+        </div>
         <List>
           <InputItem
+            className={styles.text}
             id="username"
             ref={el => this.username = el}
-            placeholder="用户名"
+            placeholder="用户名/邮箱"
           >
-            <div className={styles.iconUser} />
+            {/* <div className={styles.iconUser} /> */}
           </InputItem>
           <InputItem
+            className={styles.text}
             id="password"
             type="password"
             placeholder="密 码"
           >
-            <div className={styles.iconPwd} />
+            {/* <div className={styles.iconPwd} /> */}
           </InputItem>
         </List>
         <Button className={styles.loginBtn} type="primary" onClick={this.onSubmit}>登录</Button>
-        <div className={styles.more}>
-          <Link className={styles.fl} to=""><span>忘记密码 ?</span></Link>
-          <Link className={styles.fr} to=""><span>还没注册 ?</span></Link>
-        </div>
+
+        <Link to="" className={styles.forgetPwd}><span>忘记密码 ?</span></Link>
+
+        <Link to=""><Button type="ghost"  className={styles.registerBtn}><span>注册账号</span></Button></Link>
+        
       </div>
     )
   }
 
-  onSubmit(){
+  onSubmit() {
 
-    const username = document.getElementById("username").value; 
-    const password = document.getElementById("password").value; 
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
 
-    if (username=="") {
-      Toast.info("请输入用户名",1);
-    }else if(password==""){
-      Toast.info("请输入密码",1);
-    }else{
+    if (username == "") {
+      Toast.info("请输入用户名", 1);
+    } else if (password == "") {
+      Toast.info("请输入密码", 1);
+    } else {
       hashHistory.push('/home');
     }
   }
