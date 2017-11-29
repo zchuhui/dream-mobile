6af32e091dde1466cb30f561370d3221781848ad
zchuhui <943339564@qq.com>
2017-11-29 18:09:56 +0800

    评论

--- src/routes/home/detail.js
+++ src/routes/home/detail.js
@@ -1,67 +1,166 @@
 import React from "react";
 import { connect } from "dva";
-import { List, TextareaItem,Icon } from "antd-mobile";
+import { Link } from "dva/router";
+import { List, TextareaItem, Icon, Button, Toast, Modal } from "antd-mobile";
+import { createForm } from 'rc-form';
 import styles from "./detail.less";
+import Fly from './fly.png';
 
 class Detail extends React.Component {
-  constructor(props, context) {
-    super(props, context);
+    constructor(props, context) {
+        super(props, context);
 
-    this.state = {
-      obj: {
-        img_url: 'https://oimagec2.ydstatic.com/image?id=3678544014578067552&product=dict&_w=378&_h=322&originWidth=378&originHeight=322',
-        username: '梦人1号',
-        time: '12:00',
-        title: '我梦见了我的爷爷，非常真实',
-        content: '我在梦里看见了爷爷，爷爷对我说在梦里看见了爷爷，爷爷对我说............在梦里看见了爷爷，爷爷对我说............在梦里看见了爷爷，爷爷对我说............在梦里看见了爷爷，爷爷对我说............',
-      }
-    };
-  }
+        this.state = {
+            modal1: false,
+            placeholder: '开始评论'
+        };
+    }
 
-  componentWillMount() {
-    this.props.dispatch({ type: 'home/getDetail' });
-  }
+    componentWillMount() {
+        this.props.dispatch({ type: 'home/getDetail' });
+    }
 
-  render() {
-    return (
-      <div className={styles.detailWrap}>
-        {
-          this.props.detail && !this.props.detailLoading?
-            <div>
-              <div className={styles.item}>
-                <div className={styles.head}>
-                  <img src={this.props.detail.img_url} />
-                  <span className={styles.name}>{this.props.detail.username}</span>
-                  <span className={styles.time}>{this.props.detail.time}</span>
-                </div>
-                <div className={styles.itemContent}>
-                  <div className={styles.title}>{this.props.detail.title}</div>
-                  <div className={styles.des}>{this.props.detail.content}</div>
-                </div>
-                <div className={styles.icons}>
-                  <span className={styles.praise} onClick={() => { }}><i></i><label>{this.props.detail.praiseCount}</label></span>
-                  <span className={styles.review}><i></i><label>{this.props.detail.reviewCount}</label></span>
-                </div>
-              </div>
-              <div className={styles.reviewList}>
-                评论....
-              </div>
-            </div> : <div style={{textAlign:'center',marginTop:50}}> <Icon type="loading" size='md' /></div> 
+    componentDidMount(){
+    }
+
+
+    onWrapTouchStart = (e) => {
+        // fix touch to scroll background page on iOS
+        if (!/iPhone|iPod|iPad/i.test(navigator.userAgent)) {
+            return;
         }
+        const pNode = closest(e.target, '.am-modal-content');
+        if (!pNode) {
+            e.preventDefault();
+        }
+    }
 
-      </div>
-    )
-  }
+    showModal = (key, name) => (e) => {
+        e.preventDefault(); // 修复 Android 上点击穿透
+        this.setState({
+            [key]: true,
+            placeholder: name ? '回复 @' + name : '开始评论'
+        });
+
+    }
+    onClose = key => () => {
+        debugger
+        this.setState({
+            [key]: false,
+        });
+    }
+
+    onReview = () => {
+        const val = document.getElementById('txtId').value;
+        if (val == "") {
+            Toast.info("总得输入点什么吧？");
+        } else {
+            Toast.info("提交中...");
+
+            setTimeout(() => {
+                this.setState({
+                    "modal1": false,
+                });
+                Toast.info("提交成功");
+            }, 1500)
+        }
+    }
+
+
+    render() {
+        return (
+            <div className={styles.detailWrap}>
+                {
+                    this.props.detail && !this.props.detailLoading ?
+
+                        <div>
+                            <div className={styles.item}>
+                                <div className={styles.head}>
+                                    <img src={this.props.detail.img_url} />
+                                    <span className={styles.name}>{this.props.detail.username}</span>
+                                    <span className={styles.time}>{this.props.detail.time}</span>
+                                </div>
+                                <div className={styles.itemContent}>
+                                    <div className={styles.title}>{this.props.detail.title}</div>
+                                    <div className={styles.des}>{this.props.detail.content}</div>
+                                </div>
+                                <div className={styles.icons}>
+                                    <span className={styles.praise} onClick={() => { }}><i></i><label>{this.props.detail.praiseCount}</label></span>
+                                    <span className={styles.review}><i></i><label>{this.props.detail.reviewCount}</label></span>
+                                </div>
+                            </div>
+                            <div className={styles.reviewList}>
+                                {
+                                    this.props.detail.reviewList.map((item, index) => (
+                                        <div className={styles.reviewItem} key={index}>
+                                            <div className={styles.head}>
+                                                <img src={item.img_url} />
+                                                <span className={styles.name}>{item.username}</span>
+                                            </div>
+                                            <div className={styles.itemContent} onClick={this.showModal("modal1", item.username)}>
+                                                <div className={styles.des}>{item.reviewContent}</div>
+                                                <span className={styles.time}>{item.time}</span>
+                                            </div>
+                                        </div>
+                                    ))
+                                }
+                            </div>
+                            <div className={styles.reviewText} onClick={this.showModal("modal1")}>
+                                <input type="text" placeholder="开始评论" disabled />
+                                <img src={Fly} alt="" />
+                            </div>
+
+                            <Modal
+                                popup
+                                visible={this.state.modal1}
+                                maskClosable={false}
+                                animationType="slide-up"
+                            >
+                                <div style={{ height: 140 }}>
+                                    <TextareaItem
+                                        style={{
+                                            width: '98%',
+                                            border: '1px solid #eee',
+                                            borderRadius: 5,
+                                            marginLeft: -10,
+                                            padding: 5,
+                                            fontSize: 14,
+                                            lineHeight: '16px',
+                                            value: "",
+                                        }}
+                                        rows={4}
+                                        placeholder={this.state.placeholder}
+                                        ref={el => this.customFocusInst = el}
+                                        id="txtId"
+                                        
+                                    />
+                                    <div style={{ textAlign: 'right', width: '98%' }}>
+                                        <Button type="primary" inline size="small" style={{ marginRight: '4px' }} onClick={this.onReview}>发表</Button>
+                                        <Button type="ghost" inline size="small" style={{ marginRight: '4px' }} onClick={this.onClose('modal1')} >取消</Button>
+                                    </div>
+                                </div>
+                            </Modal>
+                        </div>
+                        :
+                        <div style={{ textAlign: 'center', marginTop: 50 }}> <Icon type="loading" size='md' /></div>
+                }
+
+            </div>
+        );
+    }
 
 
 
 }
 
 function mapStateToProps(state) {
-  console.log(state.home);
-  return {
-    ...state.home
-  };
+    return {
+        ...state.home
+    };
 }
 
-export default connect(mapStateToProps)(Detail);
+const form = createForm()(Detail)
+export default connect(mapStateToProps)(form);
+
+
+//export default connect(mapStateToProps)(Detail); 
