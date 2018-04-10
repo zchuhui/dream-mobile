b7cfeeaf781ab1781b7677024acc6aee86fdf0c8
zchuhui <943339564@qq.com>
2018-04-10 09:55:53 +0800

    调整

--- src/routes/fly/index.js
+++ src/routes/fly/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { connect } from "dva";
-import { List, TextareaItem, NavBar,Icon,Button,Toast} from "antd-mobile";
+import { List, TextareaItem, NavBar, Icon, Button, Toast } from "antd-mobile";
 import styles from "./index.less";
 import { createForm } from 'rc-form';
 
@@ -9,45 +9,37 @@ class Fly extends React.Component {
     super(props, context);
 
     this.state = {
-      face1:1,
-      face2:1,
-      face3:1,
-      feeling:null,
     };
   }
 
-  onChangeFace=(key,val)=>{
+  onChangeFace = (key, val) => {
     this.setState({
-      face1:1,
-      face2:1,
-      face3:1,
     });
 
     this.setState({
-      [key]:0,
-      feeling:val
+      [key]: 0,
+      feeling: val
     })
   }
 
-  handlePublish=()=>{
+  handlePublish = () => {
     const title = document.getElementById("titleId").value;
     const content = document.getElementById("txtId").value;
     const feeling = this.state.feeling;
 
-    if(title == ""){
-      Toast.info('请先填写标题',1);
-    }else if(content == ""){
-      Toast.info('请多少输入一点吧~~',1);
+    if (title == "") {
+      Toast.info('请先填写标题', 1);
+    } else if (content == "") {
+      Toast.info('请多少输入一点吧~~', 1);
     }
-    /* else if(feeling == null){
-      Toast.info('请选一下此刻的心情~~',1);
-    } */
-    else{
-      this.props.dispatch({type:'home/publishDream',payload:{
-        'title':title,
-        'content':content,
-        'feeling':feeling
-      }})
+    else {
+      this.props.dispatch({
+        type: 'home/publishDream', payload: {
+          'title': title,
+          'content': content,
+          'feeling': feeling
+        }
+      })
     }
   }
 
@@ -62,12 +54,6 @@ class Fly extends React.Component {
           style={{ borderBottom: "1px solid #eee" }}
         >iDream</NavBar>
 
-        {/* <div className={styles.faceWrap}>
-          <i className={this.state.face1?styles.iconfont:styles.iconfontBlue} onClick={this.onChangeFace.bind(this,'face1',2)}>&#xe609;</i>
-          <i className={this.state.face2?styles.iconfont:styles.iconfontBlue} onClick={this.onChangeFace.bind(this,'face2',1)}>&#xe791;</i>
-          <i className={this.state.face3?styles.iconfont:styles.iconfontBlue} onClick={this.onChangeFace.bind(this,'face3',0)}>&#xe608;</i>
-        </div> */}
-
         <TextareaItem
           placeholder="梦境标题"
           data-seed="logId"
@@ -77,7 +63,7 @@ class Fly extends React.Component {
           ref={el => this.customFocusInst = el}
         />
         <TextareaItem
-          {...getFieldProps('note1') }
+          {...getFieldProps('note1')}
           rows={10}
           id="txtId"
           className={styles.textarea}
@@ -95,5 +81,5 @@ function mapStateToProps(state) {
   };
 }
 
-const form =  createForm()(Fly)
+const form = createForm()(Fly)
 export default connect(mapStateToProps)(form);
